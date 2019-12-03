49d3d41e241d3897f0ffe86a2919d9b3e0aa5493
lzhmarkjen <774028218@qq.com>
2019-12-03 19:21:50 +0800

    signup完成后跳转,fix #16

--- frontend/src/Containers/Signup/index.tsx
+++ frontend/src/Containers/Signup/index.tsx
@@ -3,24 +3,27 @@ import ISignupPanel, {IFormPayLoad} from "../../Components/Signup";
 import Axios from "axios";
 import {APIList} from "../../API";
 import {message} from "antd";
+import {Redirect} from "react-router";
 
 const PageSignup = () => {
-    const [form, setForm] = useState();
+    const [login, setLogin] = useState(false);
     const handlePost = (prop: any) => {
         Axios.post(APIList.signup, prop, {withCredentials: true})
             .then(res => {
                 console.log(res);
+                setLogin(true);
             })
             .catch(() => message.error("注册失败"));
     };
-    return (
+    const form =
         <div>
             <ISignupPanel onSubmit={
                 (e: IFormPayLoad) => {
                     console.log(e);
                     handlePost(e);
                 }}/>
-        </div>
-    )
+        </div>;
+    const jump = <Redirect to={"/index"}/>;
+    return login ? jump : form;
 };
 export default PageSignup;
